a34d09b3ed3e7c6c776348342035f038d7810c6b
baris5d <baris@barisdede.com>
2022-12-04 00:37:52 +0300

    chore: add homepage items

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,26 @@
 import { Header, Footer } from '../../components/'
+import { BasketList } from '../../components/basket'
+import { Filter } from '../../components/filters/Filter'
+import { SortingFilter } from '../../components/filters/Sorting'
+import { Products } from '../../components/products'
+import { Container, Item } from '../../layouts/Container/'
 
 export function Home() {
     return (
         <>
             <Header />
+            <Container cols={12} style={{ paddingTop: '25px' }}>
+                <Item colSpan={3}>
+                    <SortingFilter />
+                    <Filter />
+                </Item>
+                <Item colSpan={6}>
+                    <Products />
+                </Item>
+                <Item colSpan={3}>
+                    <BasketList />
+                </Item>
+            </Container>
             <Footer />
         </>
     )
