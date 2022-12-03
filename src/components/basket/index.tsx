7aa7a0b64b26a039a2b5cbf086dafeb417b9b5fd
baris5d <baris@barisdede.com>
2022-12-03 23:25:52 +0300

    feat: create header and basket button component

--- src/components/basket/index.tsx
+++ src/components/basket/index.tsx
@@ -1,7 +1,31 @@
-export function Basket() {
+import { Button } from '../atoms/button'
+import { DisplayText } from '../atoms/display-text'
+import { Svg } from '../atoms/svg'
+import styles from './basket.module.scss'
+
+const BasketButton: React.FC = () => {
     return (
         <>
-            <div>Basket</div>
+            <Button className={styles.button}>
+                <Svg
+                    viewBox="0 0 24 26"
+                    width={24}
+                    height={26}
+                    className={styles.basket_button}
+                >
+                    <path d="M5.41174 9.46655H18.5884V21.0081H5.41174V9.46655Z" />
+                    <path d="M9.67188 4.65747H14.3412L15.2765 5.62625V9.4666L14.2645 9.46388V5.6242H9.75454V9.46388L8.72388 9.4666V5.6242L9.67188 4.65747Z" />
+                </Svg>
+                <DisplayText type="span" text="₺39,97" />
+            </Button>
         </>
     )
+}
+
+const BasketList: React.FC = () => {
+    return <></>
+}
+
+export const Basket: React.FC = () => {
+    return <BasketButton />
 }
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,34 @@
+import styles from './header.module.scss'
+import { Container, Item } from '../../layouts/Container'
+import Logo from '../../logo.svg'
+import { Basket } from '../basket'
 export function Header() {
     return (
         <>
-            <div>Header</div>
+            <Container cols={12} className={styles.header}>
+                <Item
+                    start={6}
+                    colSpan={2}
+                    style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                    }}
+                >
+                    <img src={Logo} alt="Market Logo" />
+                </Item>
+                <Item
+                    start={11}
+                    colSpan={2}
+                    style={{
+                        display: 'flex',
+                        justifyItems: 'center',
+                        justifyContent: 'flex-end',
+                    }}
+                >
+                    <Basket />
+                </Item>
+            </Container>
         </>
     )
 }
