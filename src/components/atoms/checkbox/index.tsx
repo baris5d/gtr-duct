0266d832d622740a0df6bca90d0b5fe1b5ade96c
baris5d <baris@barisdede.com>
2022-12-04 23:04:37 +0300

    chore: add input props

--- src/components/atoms/checkbox/index.tsx
+++ src/components/atoms/checkbox/index.tsx
@@ -2,18 +2,28 @@ import styles from './checkbox.module.scss'
 interface CheckboxProps extends React.InputHTMLAttributes<HTMLInputElement> {
     name: string
     value: string
-    checked: boolean
+    checked?: boolean
+    id?: string
+    onChange: (e: any) => void
 }
 
-export const Checkbox: React.FC<CheckboxProps> = ({ name, value, checked }) => {
+export const Checkbox: React.FC<CheckboxProps> = ({
+    name,
+    value,
+    checked,
+    onChange,
+    id,
+}) => {
     return (
         <>
             <input
                 type="checkbox"
                 name={name}
-                value={value}
                 checked={checked}
+                value={value}
+                id={id}
                 className={styles.checkbox}
+                onChange={onChange}
             />
         </>
     )
--- src/components/atoms/radiobutton/index.tsx
+++ src/components/atoms/radiobutton/index.tsx
@@ -3,17 +3,19 @@ import styles from './radio.module.scss'
 interface RadioProps extends React.InputHTMLAttributes<HTMLInputElement> {
     name: string
     value: string
+    id?: string
     checked: boolean
 }
 
 const RadioButton: React.FC<RadioProps> = (props) => {
-    const { name, value, checked, ...rest } = props
+    const { name, value, checked, id, ...rest } = props
     return (
         <>
             <input
                 type="radio"
                 name={name}
                 value={value}
+                id={id}
                 checked={checked}
                 className={styles.radio}
                 {...rest}
