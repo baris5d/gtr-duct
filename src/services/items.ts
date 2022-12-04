918e703dc66018d8d2ba3db3358bc9a0ee7dc2af
baris5d <baris@barisdede.com>
2022-12-04 23:07:14 +0300

    feat: get items from api

--- src/services/items.ts
+++ src/services/items.ts
@@ -6,8 +6,20 @@ export const itemsApi = createApi({
     reducerPath: 'itemsApi',
     baseQuery: fetchBaseQuery({ baseUrl: BASE_URL }),
     endpoints: (builder) => ({
-        getItems: builder.query<Item[], void>({
-            query: () => 'items',
+        getItems: builder.query<
+            { apiResponse: Item[]; totalCount: number; link: string },
+            any
+        >({
+            query: ({ page, filter, sort, order }) =>
+                `items?_page=${page}&_limit=16&manufacturer_like=${filter}&_sort=${sort}&_order=${order}`,
+            transformResponse: (apiResponse: Item[], meta) => ({
+                apiResponse,
+                totalCount:
+                    Number(meta?.response?.headers?.get('X-Total-Count')) || 16,
+                link: meta?.response?.headers?.get('Link') || '',
+            }),
         }),
     }),
 })
+
+export const { useGetItemsQuery } = itemsApi
